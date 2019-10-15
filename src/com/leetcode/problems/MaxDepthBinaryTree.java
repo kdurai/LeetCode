0e325a4ab1da2e47d28778605cd41858d46c616b
Durai Kasinathan <durai.kasinathan@gmail.com>
2019-10-15 11:17:59 +0530

    Next set of leetcode easy problems

--- src/com/leetcode/problems/MaxDepthBinaryTree.java
+++ src/com/leetcode/problems/MaxDepthBinaryTree.java
@@ -1,5 +1,7 @@
 package com.leetcode.problems;
 
+import java.util.Stack;
+
 import com.leetcode.datastructers.BinarySearchTree;
 import com.leetcode.datastructers.Node;
 /*
@@ -38,4 +40,26 @@ public class MaxDepthBinaryTree {
 		
 		return root == null ? 0: 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
+	
+	private static int maxDepthIterativeDFS(Node root) {
+
+		if(root == null)
+			return 0;
+		
+		Stack<Node> stack = new Stack<>();
+		Stack<Integer> value = new Stack<>();
+		stack.push(root);
+		value.push(1);
+		int max = 0;
+		
+		while(!stack.isEmpty()) {
+			
+			Node node = stack.pop();
+			int depth = value.pop();
+			
+		}
+		
+		return max;
+    }
+	
 }
