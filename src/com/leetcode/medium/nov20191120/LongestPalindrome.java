cdb8bad8d6799585202bb37013bc01462f712101
Durai Kasinathan <durai.kasinathan@gmail.com>
2019-11-20 19:23:28 +0530

    Longest Palindromic Substring

--- src/com/leetcode/medium/nov20191120/LongestPalindrome.java
+++ src/com/leetcode/medium/nov20191120/LongestPalindrome.java
@@ -1,7 +1,8 @@
 package com.leetcode.medium.nov20191120;
 
 /*
- * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+ * Given a string s, find the longest palindromic substring in s. You may assume that the 
+ * maximum length of s is 1000.
 
 Example 1:
 
