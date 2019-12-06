e2e121e681eb864fa7bbe67d2bc07bedac0c3619
Durai Kasinathan <durai.kasinathan@gmail.com>
2019-12-06 21:36:58 +0530

    Clone Graph

---
+++ src/com/leetcode/medium/dec201906/CloneGraph.java
@@ -0,0 +1,69 @@
+package com.leetcode.medium.dec201906;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+ * Given a reference of a node in a connected undirected graph, return a deep copy (clone) of the graph.
+ *  Each node in the graph contains a val (int) and a list (List[Node]) of its neighbors.
+
+Example:
+
+Input:
+{"$id":"1","neighbors":[{"$id":"2","neighbors":[{"$ref":"1"},{"$id":"3","neighbors":[{"$ref":"2"},
+{"$id":"4","neighbors":[{"$ref":"3"},{"$ref":"1"}],"val":4}],"val":3}],"val":2},{"$ref":"4"}],"val":1}
+
+Explanation:
+Node 1's value is 1, and it has two neighbors: Node 2 and 4.
+Node 2's value is 2, and it has two neighbors: Node 1 and 3.
+Node 3's value is 3, and it has two neighbors: Node 2 and 4.
+Node 4's value is 4, and it has two neighbors: Node 1 and 3.
+ 
+
+Note:
+
+The number of nodes will be between 1 and 100.
+The undirected graph is a simple graph, which means no repeated edges and no self-loops in the graph.
+Since the graph is undirected, if node p has node q as neighbor, then node q must have node p as
+ neighbor too. You must return the copy of the given node as a reference to the cloned graph.
+
+ */
+public class CloneGraph {
+	
+	static class Node {
+	    public int val;
+	    public List<Node> neighbors;
+
+	    public Node() {}
+
+	    public Node(int _val,List<Node> _neighbors) {
+	        val = _val;
+	        neighbors = _neighbors;
+	    }
+	};
+
+	private static Map<Integer, Node> map = new HashMap<>();
+	
+	public static void main(String[] args) {
+
+	}
+	
+	private static Node cloneGraph(Node node) {
+        
+		if(node == null)
+			return node;
+		
+		if(map.containsKey(node.val))
+			return map.get(node.val);
+		
+		Node clone = new Node();
+		clone.val = node.val;
+		map.put(clone.val, clone);
+		for(Node neighbor : node.neighbors)
+			clone.neighbors.add(cloneGraph(neighbor));
+		
+		return node;
+    }
+	
+}
