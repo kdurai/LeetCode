33d0ee791dd965fef6975e7bf3ff33c588002308
Durai Kasinathan <durai.kasinathan@gmail.com>
2019-12-11 14:36:37 +0530

    Find All Duplicates in an Array

---
+++ src/com/leetcode/medium/dec201911/FindDuplicates.java
@@ -0,0 +1,43 @@
+package com.leetcode.medium.dec201911;
+
+import java.util.ArrayList;
+/*
+ * Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice
+ *  and others appear once.
+
+Find all the elements that appear twice in this array.
+
+Could you do it without extra space and in O(n) runtime?
+
+Example:
+Input:
+[4,3,2,7,8,2,3,1]
+
+Output:
+[2,3]
+
+ */
+import java.util.List;
+
+public class FindDuplicates {
+
+	public static void main(String[] args) {
+
+	}
+
+	public List<Integer> findDuplicates(int[] nums) {
+	
+		List<Integer> result = new ArrayList<>();
+		
+		for(int i = 0; i < nums.length; i++) 
+		{
+			int index = Math.abs(nums[i]) - 1;
+			
+			if(nums[index] < 0)
+				result.add(Math.abs(index + 1));
+			
+			nums[index] = -nums[index];
+		}
+		return result;
+    }
+}
