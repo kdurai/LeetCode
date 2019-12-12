8449e67b169b8fdf4e9f93f70b3cad4868858226
Durai Kasinathan <durai.kasinathan@gmail.com>
2019-12-12 14:25:20 +0530

    Array Nesting

---
+++ src/com/leetcode/medium/dec201912/ArrayNesting.java
@@ -0,0 +1,60 @@
+package com.leetcode.medium.dec201912;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ * A zero-indexed array A of length N contains all integers from 0 to N-1. Find and 
+ * return the longest length of set S, where S[i] = {A[i], A[A[i]], A[A[A[i]]], ... }
+ *  subjected to the rule below.
+
+Suppose the first element in S starts with the selection of element A[i] of index = i,
+ the next element in S should be A[A[i]], and then A[A[A[i]]]… By that analogy, we stop adding right before a duplicate element occurs in S.
+
+ 
+
+Example 1:
+
+Input: A = [5,4,0,3,1,6,2]
+Output: 4
+Explanation: 
+A[0] = 5, A[1] = 4, A[2] = 0, A[3] = 3, A[4] = 1, A[5] = 6, A[6] = 2.
+
+One of the longest S[K]:
+S[0] = {A[0], A[5], A[6], A[2]} = {5, 6, 2, 0}
+ 
+
+Note:
+
+N is an integer within the range [1, 20,000].
+The elements of A are all distinct.
+Each element of A is an integer within the range [0, N-1].
+ */
+public class ArrayNesting {
+
+	public static void main(String[] args) {
+		int[] nums = {5,4,0,3,1,6,2};
+		System.out.println(arrayNesting(nums));
+
+	}
+
+	public static int arrayNesting(int[] nums) {
+        
+		int result = 0;
+		for(int i = 0 ; i < nums.length; i++)
+		{
+			int size = 0;
+			for(int k = i; nums[k] >= 0; size++)
+			{
+				int temp = nums[k];
+				nums[k] = -1;
+				k = temp;
+			}
+			
+			result = Math.max(result, size);
+		}
+		
+		return result;
+    }
+	
+}
